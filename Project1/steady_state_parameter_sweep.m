e05fe1c2ddb3a63607bc18b300806e9fdc1421af
Ian Eykamp <ian@eykamp.com>
2021-09-28 13:32:23 -0400

    modify to use parameters class

--- Project1/steady_state_parameter_sweep.m
+++ Project1/steady_state_parameter_sweep.m
@@ -1,18 +1,17 @@
 load("parameters.mat");
 
+p = parameters();
+
 insulation_thickness_range = linspace(0, 1, 10); % meters
-wall_thinckess_range = linspace(0, 1, 10); % meters
+
+insulation_thickness_sweep = zeros(length(insulation_thickness_range), 1);
 
 for it = 1:length(insulation_thickness_range)
-    insulation_thickness = insulation_thickness_range(it);
-    
-
-    function Tinside = steady_state(t, T, R_1, R_2, R_3, Q_sun, Toutside)
-        R1 = R_1 + R_2 + R_3;
-        R2 = R_4;
-        T1 = -Q_sun * (-R2 - R1) + Toutside;
-        T2 = (R1 * Toutside - T1final * R2) / (-R1 + R2)
-        
-        Tinside = T2 + (T1 - T2) * R_1 / (R_2 + R_3);
-    end
+    p.thickness_fiberglass = insulation_thickness_range(it);
+    insulation_thickness_sweep(it) = p.R_4;
 end
+
+figure(1);
+clf;
+hold on;
+plot(insulation_thickness_range, insulation_thickness_sweep);
