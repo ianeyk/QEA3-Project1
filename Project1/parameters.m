0518197f339d18e2aad45955ea1a01c380b1d5fc
Ian Eykamp <ian@eykamp.com>
2021-09-28 12:46:00 -0400

    wip

--- Project1/parameters.m
+++ Project1/parameters.m
@@ -44,6 +44,7 @@ classdef parameters
         R2 {mustBeNumeric}
 
         Q_sun {mustBeNumeric}
+        sun_angle {mustBeNumeric}
         T1i {mustBeNumeric}
         T2i {mustBeNumeric}
         Toutside {mustBeNumeric}
@@ -52,55 +53,56 @@ classdef parameters
     methods
         function obj = parameters()
             obj.A_wall = 2 * (3 * 5.1) + 2 * (4 * 5.1) * 2 * (3 * 4); % surface area of all walls, m^2
-            % obj.A_fiberglass = obj.A_wall; % m^2
-            % obj.A_floor =  2 * (5.4 * 4); % surface area of heat absorber, top and bottom, m^2
-            % obj.A_window = 4 * 3; % m^2
+            obj.A_floor =  2 * (5.4 * 4); % surface area of heat absorber, top and bottom, m^2
+            obj.A_window = 4 * 3; % m^2
 
-            % obj.thickness_wall = 0.1; % m
-            % obj.thickness_fiberglass = 0.25; % m
-            % obj.thickness_absorber = 0.1; % m
+            obj.thickness_wall = 0.1; % m
+            obj.thickness_fiberglass = 0.25; % m
+            obj.thickness_absorber = 0.1; % m
 
-            % obj.vol_brick = A_wall .* thickness_wall; % m^3
-            % obj.vol_fiberglass = A_fiberglass .* thickness_fiberglass; % m^3
-            % obj.vol_stone = A_floor .* thickness_absorber; % m^3
+            obj.rho_wall = 2000; % kg/m^3
+            obj.rho_fiberglass = 12; % kg/m^3
+            obj.rho_absorber = 3000; % kg/m^3
 
-            % obj.rho_wall = 2000; % kg/m^3
-            % obj.rho_fiberglass = 12; % kg/m^3
-            % obj.rho_absorber = 3000; % kg/m^3
+            obj.c_wall = 800; % J/kg/K
+            obj.c_fiberglass = 840; % J/kg/K
+            obj.c_absorber = 800; % J/kg/K
 
-            % obj.c_wall = 800; % J/kg/K
-            % obj.c_fiberglass = 840; % J/kg/K
-            % obj.c_absorber = 800; % J/kg/K
+            obj.k_wall = 0.6; % W/m/K
+            obj.k_fiberglass = 0.04; % W/m/K
+            obj.k_absorber = 3.0; % W/m/K
 
-            % obj.C_wall = vol_brick .* rho_wall .* c_wall;
-            % obj.C_fiberglass = vol_fiberglass .* rho_fiberglass .* c_fiberglass;
-            % obj.C_absorber = vol_stone .* rho_absorber .* c_absorber;
+            obj.h_indoor = 15; % W/m^2/K
+            obj.h_outdoor = 30; % W/m^2/K
 
-            % obj.C1 = C_absorber;
-            % obj.C2 = C_wall;
+            obj.sun_angle = 25; % degrees
+            obj.Q_sun = 300 * obj.A_window * tand(obj.sun_angle); % W
+            obj.T1i = 0;
+            obj.T2i = 0;
+            obj.Toutside = 0;
 
-            % obj.k_wall = 0.6; % W/m/K
-            % obj.k_fiberglass = 0.04; % W/m/K
-            % obj.k_absorber = 3.0; % W/m/K
+            obj.A_fiberglass = obj.A_wall; % m^2
 
-            % obj.h_indoor = 15; % W/m^2/K
-            % obj.h_outdoor = 30; % W/m^2/K
+            obj.vol_brick = obj.A_wall .* obj.thickness_wall; % m^3
+            obj.vol_fiberglass = obj.A_fiberglass .* obj.thickness_fiberglass; % m^3
+            obj.vol_stone = obj.A_floor .* obj.thickness_absorber; % m^3
 
-            % obj.R_1 = 1 ./ (h_indoor .* A_floor); % K / W
-            % obj.R_2 = 1 ./ (h_indoor .* A_fiberglass); % K / W
-            % obj.R_3 = thickness_fiberglass ./ (k_fiberglass .* A_wall); % K / W
-            % obj.R_4 = 1 ./ (h_outdoor .* A_wall); % K / W
+            obj.C_wall = obj.vol_brick .* obj.rho_wall .* obj.c_wall;
+            obj.C_fiberglass = obj.vol_fiberglass .* obj.rho_fiberglass .* obj.c_fiberglass;
+            obj.C_absorber = obj.vol_stone .* obj.rho_absorber .* obj.c_absorber;
 
-            % obj.R1 = R_1 + R_2 + R_3;
-            % obj.R2 = R_4;
+            obj.C1 = obj.C_absorber;
+            obj.C2 = obj.C_wall;
 
-            % obj.Q_sun = 300 * A_window * tand(25); % W
-            % obj.T1i = 0;
-            % obj.T2i = 0;
-            % obj.Toutside = 0;
-        end
-        function initialize(obj)
-            obj.A_fiberglass = obj.A_wall;
+
+            obj.R_1 = 1 ./ (obj.h_indoor .* obj.A_floor); % K / W
+            obj.R_2 = 1 ./ (obj.h_indoor .* obj.A_fiberglass); % K / W
+            obj.R_3 = obj.thickness_fiberglass ./ (obj.k_fiberglass .* obj.A_wall); % K / W
+            obj.R_4 = 1 ./ (obj.h_outdoor .* obj.A_wall); % K / W
+
+            obj.R1 = obj.R_1 + obj.R_2 + obj.R_3;
+            obj.R2 = obj.R_4;
+
         end
     end
 end
