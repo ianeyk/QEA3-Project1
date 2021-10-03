4171d49dc9954b97116828ea1d01d964aec765ab
Ian Eykamp <ian@eykamp.com>
2021-10-03 09:49:48 -0400

    add parameters to ode

--- Project1/initial_solar_house_ode.m
+++ Project1/initial_solar_house_ode.m
@@ -1,6 +1,6 @@
 load("parameters.mat");
 
-[ts, Ts] = ode45(@(t, y) odefun(t, y, R1, R2, C1, C2, Q_sun, Toutside), [0, 4e6], [T1i; T2i]);
+[ts, Ts] = ode45(@(t, y) odefun(t, y, R1, R2, C1, C2, Q_sun, Toutside, A_window, h_indoor, A_floor, h_outdoor), [0, 1e6], [T1i; T2i]);
 
 Tair = (Ts(:, 1) - Ts(:, 2)) .* (R_2 + R_3) ./ (R_1 + R_2 + R_3) + Ts(:, 2);
 
@@ -21,11 +21,14 @@ xlabel("Time (Weeks)");
 ylabel("Temperature (^oC)");
 legend("Floor Temperature", "Wall Temperature", "Inside Air Temperature", "location", "East");
 
-function [dTdt] = odefun(t, T, R1, R2, C1, C2, Q_sun, Toutside);
+function [dTdt] = odefun(t, T, R1, R2, C1, C2, Q_sun, Toutside, A_window, h_indoor, A_floor, h_outdoor);    
     T1 = T(1);
     T2 = T(2);
 
-    dT1dt = Q_sun ./ C1 + (T2 - T1) ./ (R1 .* C1);
+    q = -361 * cos(pi * t / (12 * 3600)) + 224 * cos(pi * t / (6 * 3600)) + 210; % W / m^2
+    Q_sun = q * A_window;
+
+    dT1dt = Q_sun ./ C1 + (T2 - T1) ./ (R1 .* C1) + (Toutside - T1) * (h_indoor * A_floor + h_indoor * A_window + .01 / .8 / A_window + h_outdoor * A_window);
     dT2dt = (Toutside - T2) ./ (R2 .* C2) - (T2 - T1) ./ (R1 .* C2);
 
     dTdt = [dT1dt; dT2dt];
