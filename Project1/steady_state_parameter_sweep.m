d491e03c1b7e664ca2f261518a90d9684231e2da
audreyabr <78472378+audreyabr@users.noreply.github.com>
2021-10-05 12:35:27 -0400

    Update steady_state_parameter_sweep.m

--- Project1/steady_state_parameter_sweep.m
+++ Project1/steady_state_parameter_sweep.m
@@ -1,10 +1,10 @@
 p = parameters();
 
 insulation_thickness_range = linspace(0, 1, 10); % meters
-wall_thickness_range = linspace(0, 1, 10); % meters
+wall_thickness_range = linspace(0, 0.5, 10); % meters
 absorber_thickness_range = linspace(0, 1, 10); % meters
 T_outside_initial_range = linspace(0, 50, 50); % C
-A_window_range = linspace(0, 20, 20); % m^2
+A_window_range = linspace(0, 12, 20); % m^2
 
 insulation_thickness_sweep = zeros(length(insulation_thickness_range), 1);
 wall_thickness_sweep = zeros(length(wall_thickness_range), 1);
@@ -27,7 +27,7 @@ for it = 1:length(absorber_thickness_sweep)
     absorber_thickness_sweep(it) = p.T_inside_final;
 end
 
-for it = 1:length(T_outside_intial_sweep)
+for it = 1:length(T_outside_initial_sweep)
     p.T_outside_initial = T_outside_initial_range(it);
     T_outside_initial_sweep(it) = p.T_inside_final;
 end
@@ -41,23 +41,38 @@ figure(1);
 clf;
 hold on;
 plot(insulation_thickness_range, insulation_thickness_sweep, 'bx');
+title("insulation thickness vs final inside temperature")
+xlabel("insulation thickness, m")
+ylabel("temperature, C")
  
 figure(2);
 clf;
 hold on;
 plot(wall_thickness_range, wall_thickness_sweep, 'bx')
+title("wall thickness vs final inside temperature")
+xlabel("wall thickness, m")
+ylabel("temperature, C")
 
 figure(3);
 clf;
 hold on;
 plot(absorber_thickness_range, absorber_thickness_sweep, 'bx')
+title("absorber thickness vs final inside temperature")
+xlabel("absorber thickness, m")
+ylabel("temperature, C")
 
 figure(4);
 clf;
 hold on;
 plot(T_outside_initial_range, T_outside_initial_sweep, 'bx')
+title("initial outside temperature vs final inside temperature")
+xlabel("initial outside temperature, C")
+ylabel("final inside temperature, C")
 
 figure(5);
 clf;
 hold on;
 plot(A_window_range, A_window_sweep, 'bx')
+title("area of window vs final inside temperature")
+xlabel("area of window, m^2")
+ylabel("final inside temperature, C")
